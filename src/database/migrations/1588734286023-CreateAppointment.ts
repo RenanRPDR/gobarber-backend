c8246c31d71162bce42f0a3bf4c4596376cc06c3
Renan Portugal do Rio <renanrpdrpro@gmail.com>
2020-05-06 00:18:52 -0300

    Ambiente configurado e criado primeira migration.

---
+++ src/database/migrations/1588734286023-CreateAppointment.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateAppointment1588734286023 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {}
+
+  public async down(queryRunner: QueryRunner): Promise<any> {}
+}
