6e4795ca7ed549b23201678835f57a5fc9dac4ff
Renan Portugal do Rio <renanrpdrpro@gmail.com>
2020-05-06 17:10:53 -0300

    Banco criado, migration executada.

--- src/database/migrations/1588734286023-CreateAppointment.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class CreateAppointment1588734286023 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<any> {}
-
-  public async down(queryRunner: QueryRunner): Promise<any> {}
-}
--- src/server.ts
+++ src/server.ts
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('🚀️ Server started on port 3333!! 🚀️');
+  console.log('🚀️ Server started on port 3333! 🚀️');
 });
