5287136e3dcf85f61d8d44ed4b34f3726685810f
Renan Portugal do Rio <renanrpdrpro@gmail.com>
2020-06-05 11:02:54 -0300

    Criando o primeiro teste 1/2.

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,20 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('shoeld be able to create a new appointment', () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '132435',
+    });
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
+});
