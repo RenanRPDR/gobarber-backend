0a04c071aa1c59ea6dc6d39576857d15d1a1cc1e
Renan Portugal do Rio <renanrpdrpro@gmail.com>
2020-06-17 11:05:57 -0300

    Teste unitario criado: Validacao do Date

--- jest.config.js
+++ jest.config.js
@@ -15,16 +15,18 @@ module.exports = {
   // cacheDirectory: "/tmp/jest_rs",
 
   // Automatically clear mock calls and instances between every test
-  clearMocks: true,
+   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+   collectCoverageFrom: [
+     '<rootDir>/src/modules/**/services/*.ts'
+   ],
 
   // The directory where Jest should output its coverage files
-  // coverageDirectory: undefined,
+   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -32,12 +34,10 @@ module.exports = {
   // ],
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: [
+    "text-summary",
+    "lcov",
+  ],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
